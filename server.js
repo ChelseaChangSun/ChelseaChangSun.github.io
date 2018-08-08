2553a8982dd60fb8d76458e873e739702c092ac5
ChelseaChangSun <40246387+ChelseaChangSun@users.noreply.github.com>
2018-08-08 17:00:01 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Set listener for changes in device orientation...');
+    window.addEventListener('deviceorientation', (event) => {
+        console.log('New Orientation:');
+        console.log('    Absolute: ' + event.absolute);
+        console.log('    Alpha   : ' + event.alpha);
+        console.log('    Beta    : ' + event.beta);
+        console.log('    Gamma   : ' + event.gamma);
+        document.getElementById('absolute').innerText = "" + event.absolute;
+        document.getElementById('alpha').innerText = "" + event.alpha;
+        document.getElementById('beta').innerText = "" + event.beta;
+        document.getElementById('gamma').innerText = "" + event.gamma;
+    }, true);
+});
