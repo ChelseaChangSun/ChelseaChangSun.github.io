34842e821ac5453d084d63c40fc5bfd17cf93bd0
ChelseaChangSun <40246387+ChelseaChangSun@users.noreply.github.com>
2018-08-31 12:36:40 -0400

    Update server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Set listener for changes in device orientation...');
-    window.addEventListener('deviceorientation', (event) => {
+    window.addEventListener('deviceorientationabsolute', (event) => {
         console.log('New Orientation:');
         console.log('    Absolute: ' + event.absolute);
         console.log('    Alpha   : ' + event.alpha);
